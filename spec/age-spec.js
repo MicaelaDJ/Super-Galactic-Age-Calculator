3c57184268cd7bb649a150f649947eb828ec491c
Guest User <Guest@epicodus-100.local>
2019-03-25 16:43:50 -0700

    try testing again, fix files

---
+++ spec/age-spec.js
@@ -0,0 +1,10 @@
+import { Earth } from './../src/age.js';
+
+describe ('Earth', function() {
+  it('returns the age of the user and remaining life expectancy in Earth years', function() {
+    let earth = new Earth(20, 100, 'You still have some years left.');
+    expect(earth.inputAge).toEqual(20);
+    expect(earth.lifeExpectancy).toEqual(100);
+    expect(earth.remainder).toEqual('You still have some years left.');
+  });
+});
---
+++ src/age.js
@@ -0,0 +1,33 @@
+export function Earth(inputAge, lifeExpectancy, remainder) {
+  this.inputAge = inputAge;
+  this.lifeExpectancy = lifeExpectancy;
+  this.remainder = remainder;
+}
+
+// /* eslint-disable no-unused-vars */
+// export default class GalacticAge {
+//   constructor(year) {
+//     this.year = year;
+//   }
+//
+//   calculateYears(age, lifeExpectancy, extraYears, planetYear) {
+//     let remainder;
+//     const remainingLife = Math.floor((lifeExpectancy - age) / planetYear);
+//     const inputAge = Math.floor(age / planetYear);
+//     if (extraYears !== 0) {
+//       remainder = Math.floor(extraYears / planetYear);
+//     }
+//     else {
+//       remainder = "You still have some years left."
+//     }
+//     const arr = [inputAge, remainingLife, remainder];
+//     return arr;
+//   }
+// }
+//
+// export class Earth extends GalacticAge {
+//   constructor() {
+//     super(1);
+//   }
+// }
+// /* eslint-disable no-unused-vars */
