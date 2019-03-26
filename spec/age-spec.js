74ba04cc9f165c67bd83117c5b487848122ab3d1
Guest User <Guest@epicodus-100.local>
2019-03-26 16:46:18 -0700

    add more tests

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,17 +1,130 @@
-import { Earth } from './../src/age.js';
+import { Calculator } from './../src/age.js';
 
-describe ('Earth', function() {
-  it('returns the age of the user and remaining life expectancy in Earth years', function() {
-    let earth = new Earth(20, 100, 'You still have some years left.');
-    expect(earth.inputAge).toEqual(20);
-    expect(earth.lifeExpectancy).toEqual(100);
-    expect(earth.remainder).toEqual('You still have some years left.');
+describe('Calculator', function() {
+
+//Earth
+  it('should return the correct age for Earth', function () {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    let age = 20;
+    expect(calculator.doTheMath(age, planet)).toEqual(20);
   });
+  it('should return the correct life expectancy for Earth', function() {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    expect(calculator.lifeExpectancy(planet)).toEqual(72)
+  });
+//Mercury
+  it('should return the correct age for Mercury', function () {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    let age = 20;
+    expect(calculator.doTheMath(age, planet)).toEqual(83.33);
+  });
+  it('should return the correct life expectancy for Mercury', function() {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    expect(calculator.lifeExpectancy(planet)).toEqual(300)
+  });
+//Venus
+  it('should return the correct age for Venus', function () {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    let age = 20;
+    expect(calculator.doTheMath(age, planet)).toEqual(31.25);
+  });
+  it('should return the correct life expectancy for Venus', function() {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    expect(calculator.lifeExpectancy(planet)).toEqual(112.5)
+  });
+//Mars
+  it('should return the correct age for Mars', function () {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    let age = 20;
+    expect(calculator.doTheMath(age, planet)).toEqual(10.64);
+  });
+  it('should return the correct life expectancy for Mars', function() {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    expect(calculator.lifeExpectancy(planet)).toEqual(38.3)
+  });
+//Jupiter
+  it('should return the correct age for Jupiter', function () {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    let age = 20;
+    expect(calculator.doTheMath(age, planet)).toEqual(1.69);
+  });
+  it('should return the correct life expectancy for Jupiter', function() {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    expect(calculator.lifeExpectancy(planet)).toEqual(6.07)
+  });
+//Saturn
+  it('should return the correct age for Saturn', function () {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    let age = 20;
+    expect(calculator.doTheMath(age, planet)).toEqual(.69);
+  });
+  it('should return the correct life expectancy for Saturn', function() {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    expect(calculator.lifeExpectancy(planet)).toEqual(2.44)
+  });
+//Uranus
+  it('should return the correct age for Uranus', function () {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    let age = 20;
+    expect(calculator.doTheMath(age, planet)).toEqual(.24);
+  });
+  it('should return the correct life expectancy for Uranus', function() {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    expect(calculator.lifeExpectancy(planet)).toEqual(.86)
+  });
+//Neptune
+  it('should return the correct age for Neptune', function () {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    let age = 20;
+    expect(calculator.doTheMath(age, planet)).toEqual(.12);
+  });
+  it('should return the correct life expectancy for Neptune', function() {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    expect(calculator.lifeExpectancy(planet)).toEqual(.44)
+  });
+//Pluto because we should appreciate all Disney characters
+  it('should return the correct age for Pluto(because it is still a planet in my heart)', function () {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    let age = 20;
+    expect(calculator.doTheMath(age, planet)).toEqual(.08);
+  });
+  it('should return the correct life expectancy for Pluto(because I do not want to offend the god of death)', function() {
+    let calculator = new Calculator();
+    let planet = 'earth';
+    expect(calculator.lifeExpectancy(planet)).toEqual(.29)
+  });
+
 });
 
-describe('Age', function() {
-  it('returns the age of the user and remaining life expectancy in Earth years', function() {
-    const earth = new Earth();
-    
-  });
-});
+// describe ('Earth', function() {
+//   it('returns the age of the user and remaining life expectancy in Earth years', function() {
+//     let earth = new Earth(20, 100, 'You still have some years left.');
+//     expect(earth.inputAge).toEqual(20);
+//     expect(earth.lifeExpectancy).toEqual(100);
+//     expect(earth.remainder).toEqual('You still have some years left.');
+//   });
+// });
+//
+// describe ('Date', function() {
+//   it('should test to see if it is a leap year', function() {
+//     let date = new Date(2019);
+//     expect(date.year(2019, 0)).toEqual(365);
+//   })
+// })
--- src/age.js
+++ src/age.js
@@ -1,31 +1,45 @@
-export function Earth(inputAge, lifeExpectancy, remainder) {
-  this.inputAge = inputAge;
-  this.lifeExpectancy = lifeExpectancy;
-  this.remainder = remainder;
+export default class Calculator {
+  constructor() {
+    this.day =
+  }
 }
 
-// export class Age {
-//   constructor(year) {
-//     this.year = year;
-//   }
-//
-//   calculateYears(age, lifeExpectancy, extraYears, planetYear) {
-//     let remainder;
-//     const remainingLife = Math.floor((lifeExpectancy - age) / planetYear);
-//     const inputAge = Math.floor(age / planetYear);
-//     if (extraYears !== 0) {
-//       remainder = Math.floor(extraYears / planetYear);
-//     }
-//     else {
-//       remainder = "You still have some years left."
-//     }
-//     const arr = [inputAge, remainingLife, remainder];
-//     return arr;
-//   }
+// export function Earth(inputAge, lifeExpectancy, remainder) {
+//   this.inputAge = inputAge;
+//   this.lifeExpectancy = lifeExpectancy;
+//   this.remainder = remainder;
 // }
 //
-// export class Earth extends Age {
-//   constructor() {
-//     super(1);
+// export class Date {
+//   constructor(year, month, day, countDays) {
+//     this.year = year;
+//     this.month = month;
+//     this.day = day;
+//     this.countDays = countDays;
+//   }
+//   year(year, month, day, countDays) {
+//     const leapYearNo = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+//     const leapYearYes = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+//     for (let i = 0; i <= this.year; i++) {
+//       if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+//         countDays+=366;
+//       } else {
+//         countDays+=365;
+//       }
+//     }
+//     for(let i=0; i <= this.month-1; i++) {
+//       if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+//         countDays+=leapYearYes[i];
+//       } else {
+//         countDays+=leapYearNo[i];
+//       }
+//     }
+//     for(let i = 0; i <= this.day-1; i++) {
+//       countDays +=1;
+//     }
+//     return countDays;
+//   }
+//   resetDays() {
+//     this.countDays = 0;
 //   }
 // }
